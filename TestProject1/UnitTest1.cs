c7dbaf21d0498a6ca7d54e1613195e60a13bac8c
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-06-21 14:17:25 +0300

    i fix all problem

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,92 @@
-namespace TestProject1
+using ATMBank;
+
+namespace BankTester
 {
+
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ReturnViewBalance()
         {
+            Program.Balance = 50.0M;
+
+            decimal result = Program.ViewBalance();
+
+            Assert.Equal(50.0M, result);
+        }
+        [Fact]
+        public void ReturnViewBalanceIfBalanceIsEqualToZero()
+        {
+            Program.Balance = 0;
+
+            decimal result = Program.ViewBalance();
+
+            Assert.Equal(0, result);
+        }
+        [Fact]
+
+        public void giveTheWithDraw30andReturnTheAmount70()
+        {
+            //arrange
+            Program.Balance = 100.0M;
+            decimal Draw = 30.0M;
+
+            //act
+            decimal result = Program.WithDraw(Draw);
+
+            // assert   
+            Assert.Equal(70.0M, result);
 
         }
+        [Fact]
+        public void GiveWithDrawSameValueOfBalance()
+        {
+            Program.Balance = 50.0M;
+
+            decimal Draw = 50.0M;
+
+            decimal result = Program.WithDraw(Draw);
+
+            Assert.Equal(0, result);
+
+        }
+        [Fact]
+        public void TestWithDrawIfTheVAlueLessThanZero()
+        {
+            Program.Balance = 50.0M;
+
+            decimal Draw = -50.0M;
+
+            decimal result = Program.WithDraw(Draw);
+
+            Assert.Equal(-1, result);
+
+        }
+        [Fact]
+
+        public void DpositAPostiveAmountToBalance()
+        {
+            Program.Balance = 20.0M;
+
+            decimal deposite = 30.0M;
+
+            decimal result = Program.Deposit(deposite);
+
+            Assert.Equal(50.0M, result);
+        }
+        [Fact]
+
+        public void DepositeANeagtiveAmountBalance()
+        {
+            // Program.Balance = 20.0M;
+
+            decimal deposite = -30.0M;
+
+            decimal result = Program.Deposit(deposite);
+
+            Assert.Equal(-1, result);
+        }
+
+
     }
 }
--- lab02-ATM/Program.cs
+++ lab02-ATM/Program.cs
@@ -1,10 +1,119 @@
-﻿namespace lab02_ATM
+﻿namespace ATMBank
 {
-    internal class Program
+    public class Program
     {
+        //write code here.
+        public static decimal Balance;
+        public static string Tracker = "";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                UserInterFace();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine(Tracker);
+            }
+
         }
+
+        public static decimal ViewBalance()
+        {
+            if (Balance < 1)
+            {
+                return 0;
+            }
+
+
+            return Balance;
+        }
+
+        public static decimal WithDraw(decimal Draw)
+        {
+            if (Draw > Balance)
+            {
+                Console.WriteLine("Cannot withDraw More Than the Balance");
+                return -1;
+            }
+            else if (Draw < 0)
+            {
+                Console.WriteLine("cannot Draw Money from in negative! ");
+                return -1;
+            }
+
+            Balance = Balance - Draw;
+            return Balance;
+
+        }
+
+        public static decimal Deposit(decimal deposit)
+        {
+            if (deposit > 0)
+            {
+                Balance = Balance + deposit;
+                return Balance;
+            }
+            else return -1;
+        }
+        public static void UserInterFace()
+        {
+            Console.WriteLine("Welcome to Our  ATM Bank ^_^");
+            Console.WriteLine($"Your Balance is  {ViewBalance()} ");
+            bool IsTrue = true;
+            while (IsTrue)
+            {
+                try
+                {
+
+                    Console.WriteLine("Please enter The Transaction : \n 1 : View Balance \t 2 : WithDraw \t 3 : Deposit \t  4 : Exit the Atm ");
+
+                    int transaction = Convert.ToInt32(Console.ReadLine());
+
+                    if (transaction == 1)
+                    {
+                        Console.WriteLine($"Your Balance : {ViewBalance()} ");
+
+                    }
+                    else if (transaction == 2)
+                    {
+                        Console.WriteLine($"Enter A value to Draw from the ${ViewBalance()}  ");
+                        decimal Draw = Convert.ToDecimal(Console.ReadLine());
+                        WithDraw(Draw);
+                        Console.WriteLine($"Now Your Balance is : {ViewBalance()} ");
+                        Tracker += $"You Draw {Draw} From your Balance\n";
+                    }
+                    else if (transaction == 3)
+                    {
+                        Console.WriteLine($"Enter A value to Deposit Value to Your Balance : ${ViewBalance()} ... ");
+                        decimal deposit = Convert.ToDecimal(Console.ReadLine());
+                        Deposit(deposit);
+                        Console.WriteLine($"Now Your Balance is : {ViewBalance()} ");
+                        Tracker += $"You Deposit {deposit} in your Balance\n";
+                    }
+                    else if (transaction == 4)
+                    {
+                        IsTrue = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input Try Again please ^_^");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong input data Type please make sure it is number ^_^");
+                }
+            }
+
+
+
+        }
+
+
     }
 }
